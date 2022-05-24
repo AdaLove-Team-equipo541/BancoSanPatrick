64318da2c275eef328843dc0ffe9da7dfebd8336
Jose Luis <jose.luis.sandoval@pittaj.com>
2022-05-24 00:18:21 -0500

    created Response Wrapper and identity service extension

---
+++ server/src/SanPatrick.Application/Wrappers/Response.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SanPatrick.Application.Wrappers
+{
+    public class Response<T>
+    {
+        public bool Succesed { get; set; }
+
+        public string? Message { get; set; }
+
+        public List<string>? Errors { get; set; }
+
+        public T? Data { get; set; }
+
+        public Response()
+        {
+
+        }
+        public Response(T data, string message = null)
+        {
+            Succesed = true;
+            Message = message;
+            Data = data;
+        }
+        public Response(string message)
+        {
+            Succesed = false;
+            Message = message;
+        }
+    }
+}
---
+++ server/src/SanPatrick.Identity/IdentityServiceExtensions.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using SanPatrick.Application.Wrappers;
+using SanPatrick.Domain.Services.Authentication;
+using SanPatrick.Identity.Contexts;
+using SanPatrick.Identity.Models;
+using System.Text;
+
+namespace SanPatrick.Identity
+{
+    public static class IdentityServiceExtensions
+    {
+        public static void AddIdentityServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<IdentityContext>(options => options.UseNpgsql(
+                configuration.GetConnectionString("IdentityConnection"),
+                b => b.MigrationsAssembly(typeof(IdentityContext).Assembly.FullName)
+            ));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<IdentityContext>().AddDefaultTokenProviders();
+            services.Configure<JwtSettings>(configuration.GetSection("JwtSettings"));
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(o =>
+            {
+                o.RequireHttpsMetadata = false;
+                o.SaveToken = false;
+                o.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero,
+                    ValidIssuer = configuration["JwtSettings:Issuer"],
+                    ValidAudience = configuration["JwtSettings:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtSettings:Key"]))
+                };
+
+                o.Events = new JwtBearerEvents()
+                {
+                    OnAuthenticationFailed = c =>
+                    {
+                        c.NoResult();
+                        c.Response.StatusCode = 500;
+                        c.Response.ContentType = "text/plain";
+                        return c.Response.WriteAsync(c.Exception.ToString());
+                    },
+
+                    OnChallenge = context =>
+                    {
+                        context.HandleResponse();
+                        context.Response.StatusCode = 401;
+                        context.Response.ContentType = "application/json";
+                        var result = JsonConvert.SerializeObject(new Response<string>("Usted no esta autorizado"));
+                        return context.Response.WriteAsync(result);
+                    },
+
+                    OnForbidden = context =>
+                    {
+                        context.Response.StatusCode = 400;
+                        context.Response.ContentType = "application/json";
+                        var result = JsonConvert.SerializeObject(new Response<string>("Usted no esta tiene permisos sobre este recurso"));
+                        return context.Response.WriteAsync(result);
+                    }
+                };
+            });
+        }
+    }
+}
