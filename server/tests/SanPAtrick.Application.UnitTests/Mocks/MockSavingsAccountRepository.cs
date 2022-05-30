b3f24d0f397249defdb43722274110804523ee1a
Jose Luis <jose.luis.sandoval@pittaj.com>
2022-05-29 21:15:28 -0500

    added test project and depenncies

---
+++ server/tests/SanPAtrick.Application.UnitTests/Mocks/MockSavingsAccountRepository.cs
@@ -0,0 +1,14 @@
+﻿using Moq;
+using SanPatrick.Application.Interfaces.Persistence;
+using SanPatrick.Domain.Entities;
+
+namespace SanPAtrick.Application.UnitTests.Mocks
+{
+    public static class MockSavingsAccountRepository
+    {
+        public static Mock<IRepositoryAsync<SavingsAccount>> GetSavingsAccountRepository()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
