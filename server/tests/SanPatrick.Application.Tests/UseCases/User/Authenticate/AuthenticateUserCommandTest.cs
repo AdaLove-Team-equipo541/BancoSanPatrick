26455052797fb81cf3bc4503cb723aebe347c8ab
Juan M Palazzolo <juanmartinpalazzolo@icloud.com>
2022-06-06 18:24:16 +0200

    Added unit testing to commands

---
+++ server/tests/SanPatrick.Application.Tests/UseCases/User/Authenticate/AuthenticateUserCommandTest.cs
@@ -0,0 +1,28 @@
+﻿using SanPatrick.Application.UseCases.User.Authenticate;
+using FluentAssertions;
+using Xunit;
+
+namespace SanPatrick.Application.Tests.UseCases.User.Authenticate;
+
+public class AuthenticateUserCommandTest
+{
+    [Fact]
+    public void AuthenticateUserCommand_HasRequiredFields_Test()
+    {
+        // Arrange
+        const string email = "test@test.com";
+        const string password = "Test_123";
+        const string ipAddress = "192.0.0.1";
+        var authenticateUserCommand = new AuthenticateUserCommand(
+            email, 
+            password, 
+            ipAddress);
+        
+        // Act
+        
+        // Assert
+        authenticateUserCommand.Email.Should().Be(email);
+        authenticateUserCommand.Password.Should().Be(password);
+        authenticateUserCommand.IpAddress.Should().Be(ipAddress);
+    }
+}
---
+++ server/tests/SanPatrick.Application.Tests/UseCases/User/Register/RegisterUserCommandTest.cs
@@ -0,0 +1,68 @@
+﻿using FluentAssertions;
+using SanPatrick.Application.UseCases.User.Register;
+using Xunit;
+
+namespace SanPatrick.Application.Tests.UseCases.User.Register;
+
+public class RegisterUserCommandTest
+{
+    [Fact]
+    public void RegisterUserCommand_HasRequiredFields_Test()
+    {
+        // Arrange
+        const string email = "test@test.com";
+        const string password = "Test_123";
+        const string confirmPassword = "Test_123";
+        const string phoneNumber = "1234566789";
+        const string firstName = "Firstname";
+        const string middleName = "Middlename";
+        const string lastName = "Lastname";
+        const string birthDate = "10/12/1990";
+        const string avatarString = "url";
+        const string citizenship = "col";
+        const int maritalStatus = 1;
+        const string occupation = "developer";
+        const string country = "col";
+        const string city = "bog";
+        const string address = "add 1";
+        const string origin = "origin";
+        var registerUserCommand = new RegisterUserCommand(
+            email,
+            password,
+            confirmPassword,
+            phoneNumber,
+            firstName,
+            middleName,
+            lastName,
+            birthDate,
+            avatarString,
+            citizenship,
+            maritalStatus,
+            occupation,
+            country,
+            city,
+            address,
+            origin
+        );
+
+        // Act
+        
+        // Assert
+        registerUserCommand.Email.Should().Be(email);
+        registerUserCommand.Password.Should().Be(password);
+        registerUserCommand.ConfirmPassword.Should().Be(confirmPassword);
+        registerUserCommand.PhoneNumber.Should().Be(phoneNumber);
+        registerUserCommand.FirstName.Should().Be(firstName);
+        registerUserCommand.MiddleName.Should().Be(middleName);
+        registerUserCommand.LastName.Should().Be(lastName);
+        registerUserCommand.BirthDate.Should().Be(birthDate);
+        registerUserCommand.AvatarString.Should().Be(avatarString);
+        registerUserCommand.Citizenship.Should().Be(citizenship);
+        registerUserCommand.MaritalStatus.Should().Be(maritalStatus);
+        registerUserCommand.Occupation.Should().Be(occupation);
+        registerUserCommand.Country.Should().Be(country);
+        registerUserCommand.City.Should().Be(city);
+        registerUserCommand.Address.Should().Be(address);
+        registerUserCommand.Origin.Should().Be(origin);
+    }
+}
